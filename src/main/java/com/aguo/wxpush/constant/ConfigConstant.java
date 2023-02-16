b0ae3e4e04ba05b00b71ad6dd9332a9c71ea63a3
maolei <86232506@qq.com>
2023-02-16 18:03:16 +0800

    配置信息更改

--- src/main/java/com/aguo/wxpush/constant/ConfigConstant.java
+++ src/main/java/com/aguo/wxpush/constant/ConfigConstant.java
@@ -2,11 +2,12 @@ package com.aguo.wxpush.constant;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 
-@Component
+@Configuration
 @ConfigurationProperties(prefix = "wx.config")
 public class ConfigConstant {
     @Value("${wx.config.appId}")
